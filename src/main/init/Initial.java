8eca8077e6a7c9a353c95f617975936666c464f5
丁斯也 <kaka940307@vip.qq.com>
2018-06-06 23:35:17 +0800

    pull就完事了

--- src/main/init/Initial.java
+++ src/main/init/Initial.java
@@ -1,11 +1,8 @@
 package main.init;
 
-import constant.Constant;
 import model.node.Node;
 import node.communication.Receiver;
 import client.Client;
-import org.apache.commons.lang3.SystemUtils;
-import util.Sign;
 import util.simulator.Simulator;
 
 public class Initial {
