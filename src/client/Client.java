0c5713fd326bd888302111ecebe8d4ab5992928a
siye.dsy <siye.dsy@alibaba-inc.com>
2018-06-28 20:54:12 +0800

    上传

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,8 +1,18 @@
 package client;
 
 import com.alibaba.fastjson.JSON;
+import constant.Constant;
+import main.Main;
+import model.annotation.MulThreadShareData;
+import model.block.Block;
 import node.communication.udp.UDP_Sender;
 import model.record.Record;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.TimeUnit;
 
 /**
  * Created by DSY on 2018/5/10.
@@ -10,9 +20,52 @@ import model.record.Record;
  * 区块链节点的一部分功能实现。
  *
  * 客户端功能：（1）接受用户从雷达的输入，将数据在区块链中存储
+ * （2）确保每条数据都已经被区块链确认，只在最新的两个区块中查找（如果两个区块中没有，则认为数据已经丢失）
  */
-public class Client{
+public class Client implements Runnable{
+    @MulThreadShareData
+    private static Set<Record> confirming = ConcurrentHashMap.newKeySet();
+
+    private static List<Record> warning = new ArrayList<>();
+
+    private static List<Record> lost = new ArrayList<>();
+
     static void send(Record record) {
+        confirming.add(record);
         UDP_Sender.broadcast("<record>" + JSON.toJSONString(record));
+    }
+
+    @Override
+    public void run() {
+        while (Main.isRunning()){
+            try {
+                TimeUnit.SECONDS.sleep(Constant.HALF_BLOCK_GAP * 2);
+                List<List<Block>> blockchains = getBlockChains();
+                for (Record record : confirming){
+                    if (onChain(record)){
+                        confirming.remove(record);
+                    }
+                }
+                for (Record record : warning){
+                    if (!onChain(record)){
+                        lost.add(record);
+                    }
+                }
+                warning = new ArrayList<>(confirming);
+                for (Record record : lost){
+                    Client.send(record);
+                }
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private boolean onChain(Record record){
+
+    }
+
+    private List<List<Block>> getBlockChains(){
+
     }
 }
